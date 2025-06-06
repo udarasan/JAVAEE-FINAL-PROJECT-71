32345003a2902cce0ea4f71f534758da59b8ccd2
Udara San <udarassanjeewa@gmail.com>
2025-06-06 15:05:34 +0530

    add signup

---
+++ EMS-FN/js/signup.js
@@ -0,0 +1,30 @@
+$('#sign-up-btn').on('click', function() {
+    var name = $('#name').val();
+    var email = $('#email').val();
+    var password = $('#password').val();
+
+    if (name && email && password) {
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8080/EMS_Web_exploded/api/v1/signup',
+            data: JSON.stringify({ 
+                uname: name,
+                uemail: email, 
+                upassword: password 
+            }),
+            contentType: 'application/json',
+            success: function(response) {
+                if (response.success) {
+                    window.location.href = 'signin.html';
+                } else {
+                    $('#error-message').text(response.message).show();
+                }
+            },
+            error: function() {
+                $('#error-message').text('An error occurred. Please try again.').show();
+            }
+        });
+    } else {
+        $('#error-message').text('Please enter both email and password.').show();
+    }
+})
